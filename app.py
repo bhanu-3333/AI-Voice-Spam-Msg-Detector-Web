2cabf5940142acd9fb0faf31ae8425b69aefe07e
bhanu-3333 <bhanuu339@gmail.com>
2025-12-14 12:57:31 +0530

    Switch to Hugging Face API to reduce memory

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import os
-
+import requests
 from flask import Flask, render_template, request, jsonify
-from transformers import pipeline
 
 app = Flask(__name__)
 
-classifier = pipeline(
-    "text-classification",
-    model="cybersectony/phishing-email-detection-distilbert_v2.1"
-)
+HF_API_URL = "https://router.huggingface.co/hf-inference/models/cybersectony/phishing-email-detection-distilbert_v2.1"
+HF_TOKEN = os.environ.get("HF_TOKEN")  # set in Render
+
+headers = {
+    "Authorization": f"Bearer {HF_TOKEN}"
+}
 
 @app.route("/")
 def home():
@@ -21,8 +22,15 @@ def checker():
 @app.route("/predict", methods=["POST"])
 def predict():
     text = request.form.get("email")
-    result = classifier(text)
-    return jsonify(result)
+
+    response = requests.post(
+        HF_API_URL,
+        headers=headers,
+        json={"inputs": text}
+    )
+
+    return jsonify(response.json())
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
